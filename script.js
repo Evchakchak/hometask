a14b6ec2c0a129b2699dd9bad287f186a538a0ed
Eva <mmej47770@gmail.com>
2024-10-22 18:15:30 +0300

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Елисеева Ева";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
